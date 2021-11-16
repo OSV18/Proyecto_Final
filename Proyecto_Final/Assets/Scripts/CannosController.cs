c9bc25f6d8a4fe32dffe196a2ab15ff8b4d73264
ocmir <90803042+OSV18@users.noreply.github.com>
2021-11-15 22:34:44 -0300

    15/11

---
+++ Proyecto_Final/Assets/Scripts/CannosController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] GameObject[] cannons;
+    [SerializeField] GameObject selected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DiseableCannos();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void DiseableCannos()
+    {
+        for (int i = 0; i < cannons.Length; i++)
+        {
+            cannons[i].SetActive(false);
+
+        }
+    }
+
+    /*private void ActiveOne()
+    {
+        int cannonInde = Random.Range(0, cannons.Length);
+        get
+
+
+    }
+
+    /*private CannonController GetCannonComponent(GameObject.target)
+    {
+        return cannon.GetComponent
+    }
+}
