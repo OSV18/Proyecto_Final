b972433445d2d7492986103f158a3560a681a8c8
ocmir <90803042+OSV18@users.noreply.github.com>
2021-12-12 20:53:28 -0300

    Post Salgado

--- Proyecto_Final/Assets/Scripts/Player/PlayerContoller.cs
+++ Proyecto_Final/Assets/Scripts/Player/PlayerContoller.cs
@@ -6,30 +6,34 @@ using UnityEngine.UI;
 
 public class PlayerContoller : MonoBehaviour
 {
+    [SerializeField] private float life = 100;
     [SerializeField] float SpeedPlayer = 4F;
-    float CameraAxis = 180f;
-    [SerializeField] float speedTun;
+    [SerializeField] private Vector3 velocity;
+
+    [SerializeField] private Transform cam;
+    [SerializeField] float mouseSesitivity = 2f;
     [SerializeField] Animator animaPlayer;
-    [SerializeField] float jumpForce;
-    [SerializeField] LayerMask groundLayer;
     [SerializeField] private Image lifeBar;
     [SerializeField] private GameObject myObjetct;
-    [SerializeField] private float life = 100;
+    private float Gavity = -9.81f;
+
 
     private InventoryManagers mgInventory;
     private Rigidbody rbPlayer;
+    private CharacterController cc;
 
     //EVENT
     public static event Action onDeath;
     public static event Action<int> onScore;
+    public static event Action<bool> onDamage;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        cc = GetComponent<CharacterController>();
         animaPlayer.SetBool("isRun", false);
         //animaPlayer.SetBool("isFire", false);
-        rbPlayer = GetComponent<Rigidbody>();
         mgInventory = GetComponent<InventoryManagers>();
         
     }
@@ -37,13 +41,15 @@ public class PlayerContoller : MonoBehaviour
 
     void FixedUpdate()
     {
-        Move();
-        RotatePlayer();
+
         
     }
     // Update is called once per frame
     void Update()
     {
+        Move();
+        RotatePlayer();
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (isGrounded)
@@ -52,9 +58,7 @@ public class PlayerContoller : MonoBehaviour
             }
             
         }
-        
-        
-
+                
         if (Input.GetButtonDown("Fire1"))
         {
             animaPlayer.SetBool("isAttack", true);
@@ -67,19 +71,19 @@ public class PlayerContoller : MonoBehaviour
         life = Mathf.Clamp(life, 0, 100);
         lifeBar.fillAmount = life / 100;
 
+        velocity.y += Gavity * Time.deltaTime;
+        cc.Move(velocity * Time.deltaTime);
+
 
 
     }
 
     private void RotatePlayer()
     {
-        if(Input.GetAxis("Mouse X") != 0)
-        { 
-         
-            CameraAxis += Input.GetAxis("Mouse X");
-            Quaternion Angle = Quaternion.Euler(0, CameraAxis * speedTun, 0);
-            transform.rotation = Angle;
-        }
+        float horizontalRotation = Input.GetAxis("Mouse X");
+        transform.Rotate(0, horizontalRotation * mouseSesitivity, 0);
+
+        
     }
 
     private void Move()
@@ -87,12 +91,15 @@ public class PlayerContoller : MonoBehaviour
         float ejeHorizontal = Input.GetAxis("Horizontal");
         float ejeVertical = Input.GetAxis("Vertical");
 
-        if (ejeHorizontal != 0 || ejeVertical != 0)
+        Vector3 direction = new Vector3(ejeHorizontal, 0, ejeVertical).normalized;
+        
+        if(direction.magnitude >= 0.1f)
         {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
             animaPlayer.SetBool("isRun", true);
-            rbPlayer.AddRelativeForce(Vector3.forward * SpeedPlayer * ejeVertical, ForceMode.Acceleration);
-            rbPlayer.AddRelativeForce(Vector3.right * SpeedPlayer * ejeHorizontal, ForceMode.Acceleration);
-        }
+            cc.Move(moveDir.normalized * SpeedPlayer * Time.deltaTime);
+        }        
 
         else
         {
@@ -132,7 +139,7 @@ public class PlayerContoller : MonoBehaviour
     private void Jump()
     {
         Debug.Log("salta");
-        rbPlayer.AddForce(0, 1 * jumpForce, 0);
+        velocity.y = Mathf.Sqrt(-5f * Gavity);
 
     }
 
@@ -154,10 +161,14 @@ public class PlayerContoller : MonoBehaviour
         {
             Debug.Log("Golpeado por Enemy");
             life -= 5f;
+            onDamage?.Invoke(true);
+
             if (life == 0)
             {
+                
                 onDeath?.Invoke();
                 myObjetct.SetActive(false);
+                
             }
         }
         /*if (collision.contacts[0].otherCollider.gameObject.CompareTag("Hand Enemy"))
@@ -170,6 +181,13 @@ public class PlayerContoller : MonoBehaviour
                 onDeath?.Invoke();
             }
         }*/
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            onDamage?.Invoke(false);
+        }
     }
 
 
